7fcd988f2874e48e6ddeb7a0abd171aed2a24f16
TAKANO Mitsuhiro <takano32@gmail.com>
2022-07-09 07:11:00 +0900

    first commit

---
+++ lib/hatena/bookmark.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "bookmark/version"
+
+module Hatena
+  module Bookmark
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
